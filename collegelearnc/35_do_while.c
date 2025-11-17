4525faf73212e417c6cfdcecc3f0aba603b3c3a2
praveen k <praveenk53ngup@gmail.com>
2025-11-17 14:51:59 +0530

    Add files via upload

---
+++ collegelearnc/35_do_while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1,sum=0;
+    printf("enter a positive integer: ");
+    scanf("%d", &n);
+    do
+    {
+        sum = sum+i;
+        i++; 
+    
+    }
+    while(i<=n);
+    printf("sum=%d \n",sum);
+    return 0;
+    
+}
